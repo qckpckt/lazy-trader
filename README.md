b97096cd0e6550dc70b915d6aa1599f21fec874e
Alex Myers <qckpckt@gmail.com>
2022-07-08 15:38:46 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # lazy-trader
+
+This is a simple script designed to be run on a schedule that uses a supplied config file to figure out what stocks you should buy.
+
+## config file explanation
+
+The config file has two sections: `auth` and `allocation_map`:
+
+- `auth` contains the currently valid refresh token.
+- `allocation_map` contains a map of several classes of allocation.
+
+Each allocation uses the same schema:
+
+```yaml
+bond: # the name of the allocation
+    allocation: 10 # the target allocation percentage
+    symbols: # a list of symbols you currently own that match this allocation (copy the next two lines for each new symbol)
+    - currency: '' # the currency of the symbol
+      symbol: '' # the ticker symbol (eg VCN.TO) <- Note you'll want the .TO for symbols that are also traded on the US exchange!
+    buy: '' # which symbol to buy, should your account be behind your target allocation percentage. NOTE - This should be a symbol that exists in the `symbols` array, even if you don't own any of it, otherwise subsequent buys won't update your allocation!
+```
+## setup
+
+Here's how to configure this app to work with your account: 
+
+- Make a copy of the config.template.yaml file, name it `config.yaml`
+- Enable API access on your questrade account
+- Create a new app, generate a refresh token, paste that in `auth.refresh_token` in your `config.yaml`
+
+Now for the time-consuming part. You're going to need to go through each of your questrade accounts, and research each ticker symbol to figure out which category you need to add it to. Make sure to add the currency as well as the ticker symbol
+
+If it turns out that the categories in the yaml file don't make sense to you, you can create new ones - just make sure the category has the same schema.
+
+## usage
+
+### prerequisites
+
+- A version of `python3`
+
+### running the code
+
+Follow these steps first:
+
+- `python3 -m virtualenv venv`
+- `source venv/bin/activate`
+- `pip install -r requirements.txt`
+
+To run the code, once you have set up `config.yaml`:
+
+```
+python app/main.py
+```
+
+## TODO
+- dockerize project
+- create makefile
+- make it easy to set up auto scheduling with a cronjob
+- add method that emails results instead of pprint.
---
+++ app/main.py
@@ -0,0 +1,117 @@
+
+from collections import defaultdict
+from pprint import pprint
+
+import requests
+from requests.exceptions import HTTPError
+import yaml
+
+
+class LazyError(Exception):
+    pass
+
+
+class LazyTrader:
+
+    def __init__(self):
+
+        with open("app/config.yaml", "r") as configfile:
+            self.config = yaml.safe_load(configfile)
+            self.refresh_token = self.config["auth"]["refresh_token"]
+            self.allocation_map = self.config["allocation_map"]
+
+        self.refresh_token_url = "https://login.questrade.com/oauth2/token"
+        self.session = requests.Session()
+        self._get_credentials()
+        self.retries = 3
+
+    def _get_credentials(self):
+        params = {
+            "grant_type": "refresh_token",
+            "refresh_token": self.refresh_token
+        }
+        resp = self.session.get(self.refresh_token_url, params=params)
+        resp.raise_for_status()
+        resp_json = resp.json()
+        self.api_server = resp_json.get("api_server")
+        self.access_token = resp_json.get("access_token")
+        self.refresh_token = resp_json.get("refresh_token")
+        print(f"NEW REFRESH TOKEN = {self.refresh_token}")
+        pprint(f"NEW REFRESH TOKEN = {self.refresh_token}")
+
+    def _request(self, name: str, path: str, *args, **kwargs):
+        """generic request method"""
+        method = getattr(self.session, name)
+        url = f"{self.api_server}{path}"
+        headers = {
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        for i in range(self.retries):
+            try:
+                response = method(url, *args, headers=headers, **kwargs)
+                if response.status_code == 401:
+                    self._get_credentials()
+                response.raise_for_status()
+                return response.json()
+            except HTTPError as e:
+                pprint(e)
+                continue
+
+    def get(self, path: str):
+        return self._request("get", path)
+
+    def _fetch_account_allocations(self, positions: list) -> tuple:
+        asset_class_totals = defaultdict(float)
+        currency_totals = defaultdict(float)
+        for asset_class in self.allocation_map:
+            for symbol in self.allocation_map[asset_class]["symbols"]:
+                for position in positions:
+                    if position["symbol"] == symbol["symbol"]:
+                        asset_class_totals[f"{asset_class}_{symbol['currency']}"] += position["currentMarketValue"]
+                        currency_totals[f"{symbol['currency']}"] += position["currentMarketValue"]
+        return asset_class_totals, currency_totals
+
+    def _calculate_allocation_variances(self, positions: list) -> dict:
+        asset_class_totals, currency_totals = self._fetch_account_allocations(positions)
+        return {
+            k: (self.allocation_map[k.split("_CAD")[0]]["allocation"] - int(v / currency_totals["CAD"] * 100))
+            for (k, v) in asset_class_totals.items()
+            if "CAD" in k
+        }
+
+    def get_highest_allocation_variance(self, account: dict) -> str:
+        positions = self.get(f"v1/accounts/{account['number']}/positions")["positions"]
+        variances = self._calculate_allocation_variances(positions)
+        highest_variance = sorted(variances.items(), key=lambda item: item[1])[-1][0]
+        return {
+            "account": account["type"],
+            "variances": variances,
+            "highest_variance": highest_variance,
+            "suggested_buy": self.allocation_map[highest_variance.split("_CAD")[0]]["buy"]
+        }
+
+    def save_config(self):
+        """Store latest refresh token before exiting."""
+        self.config["auth"]["refresh_token"] = self.refresh_token
+        with open("app/config.yaml", "w") as outfile:
+            yaml.safe_dump(self.config, outfile)
+
+
+def lazy_trade(lt: LazyTrader, account_allocations: list):
+    pass
+
+
+def main():
+    lt = LazyTrader()
+    try:
+        accounts = lt.get("v1/accounts")["accounts"]
+        trade_recommendations = [lt.get_highest_allocation_variance(account) for account in accounts]
+        pprint(trade_recommendations)
+        # lazy_trade(lt, account_allocations)
+    finally:
+        lt.save_config()
+
+
+if __name__ == "__main__":
+    main()
